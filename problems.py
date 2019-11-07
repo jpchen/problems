96619cf96e963e40a135e24018938dd0985c28cc
jpchen <jonathanp.chen@gmail.com>
2019-11-07 14:47:28 -0800

    sources

--- problems.py
+++ problems.py
@@ -331,6 +331,38 @@ def test_bfs():
 
 # ======================================
 # snapchat and tesla
+# overview: http://cs231n.github.io/convolutional-networks/
+# code implementation: https://victorzhou.com/blog/intro-to-cnns-part-1/
+
+def iterate_regions(image):
+'''
+Generates all possible 3x3 image regions using valid padding.
+- image is a 2d numpy array
+'''
+h, w = image.shape
+
+for i in range(h - 2):
+  for j in range(w - 2):
+    im_region = image[i:(i + 3), j:(j + 3)]
+    yield im_region, i, j
+
+
+def forward(input, num_filters):
+'''
+Performs a forward pass of the conv layer using the given input.
+Returns a 3d numpy array with dimensions (h, w, num_filters).
+- input is a 2d numpy array
+'''
+filters = np.random.randn(num_filters, 3, 3)
+h, w = input.shape
+output = np.zeros((h - 2, w - 2, num_filters))
+
+for im_region, i, j in iterate_regions(input):
+  output[i, j] = np.sum(im_region * filters, axis=(1, 2))
+
+return output
+
+
 def conv_2d(input_tensor, weight, kernel_size):
     # FIXME this doesnt work
     h_out = input_tensor.shape[2] - weight.shape[0] + 1
